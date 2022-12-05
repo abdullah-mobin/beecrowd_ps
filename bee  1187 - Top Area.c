b0fe32bc78a924e805eefb769441033540605617
Md Abdullah Al Mobin <112345262+abdullah-mobin@users.noreply.github.com>
2022-12-05 17:54:37 +0600

    Create bee  1187 - Top Area.c

---
+++ bee  1187 - Top Area.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    double arr[12][12],s = 0.0;
+    char x;
+    int i,j;
+    scanf("%c",&x);
+    for( i = 0; i < 12; i++){
+            for( j = 0; j < 12; j++)
+            {
+                    scanf("%lf",&arr[i][j]);
+                    if(j > i && (j+i) < 11)
+                    {
+                        s = s+ arr[i][j];
+                    }
+            }
+   }
+    
+    if(x == 'S')
+         printf("%.1f\n", s);
+    else
+         printf("%.1f\n", s/30.0);
+    return 0;
+}
