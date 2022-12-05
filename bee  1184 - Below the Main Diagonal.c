0607daa2dbaf483b21ae55b628a252a7e62102bd
Md Abdullah Al Mobin <112345262+abdullah-mobin@users.noreply.github.com>
2022-12-05 18:19:41 +0600

    Create bee  1184 - Below the Main Diagonal.c

---
+++ bee  1184 - Below the Main Diagonal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    double arr[12][12],s = 0.0;
+    char x;
+    int i,j;
+    scanf("%c",&x);
+    for( i = 0; i < 12; i++){
+        for( j = 0; j < 12; j++)
+        {
+            scanf("%lf",&arr[i][j]);
+            if(j<i)
+                s = s+ arr[i][j];
+                
+        }
+   }
+    
+    if(x == 'S')
+         printf("%.1f\n", s);
+    else
+         printf("%.1f\n", s/66.0);
+    return 0;
+}
