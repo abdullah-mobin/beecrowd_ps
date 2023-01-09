d45bbbb28c0acf1f708b4d1055a04865efd0f995
Md Abdullah Al Mobin <112345262+abdullah-mobin@users.noreply.github.com>
2023-01-09 20:18:50 +0600

    Create  1564 - Brazil World Cup.c

---
+++ 1564 - Brazil World Cup.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+	  while(scanf("%d",&x)==1) 
+    {
+		    if (x == 0)
+		  	printf("vai ter copa!\n");
+        else
+			  printf("vai ter duas!\n");
+	  }
+    return 0;
+}
