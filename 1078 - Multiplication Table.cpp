933e3b6c43f01a5d6a3c07fc7dfbc412f7251903
Md Abdullah Al Mobin <112345262+abdullah-mobin@users.noreply.github.com>
2023-01-15 00:45:00 +0600

    Create 1078 - Multiplication Table.cpp

---
+++ 1078 - Multiplication Table.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+	int x;
+	cin >> x ;
+	for (int i = 1; i <=10; i++)
+		cout << i << " x " << x << " = " << i*x << endl;
+	return 0;
+}
