e85dd39e55bac3e61eda6bf726a41b1ca6199ebb
Md Abdullah Al Mobin <112345262+abdullah-mobin@users.noreply.github.com>
2023-01-31 21:08:44 +0600

    Create 1071 - Sum of Consecutive Odd Numbers I.cpp

---
+++ 1071 - Sum of Consecutive Odd Numbers I.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	int a, b, sum = 0;
+	cin >> a >> b;
+	if (a < b)
+	{
+
+		for (int i = a + 1; i < b; i++)
+		{
+			if (i % 2 == 1 || i % 2 == -1)
+			{
+				sum = sum + i;
+			}
+		}
+	}
+	else
+	{
+		for (int i = b + 1; i < a; i++)
+		{
+			if (i % 2 == 1 || i % 2 == -1)
+			{
+				sum = sum + i;
+			}
+		}
+	}
+
+	cout << sum << endl;
+
+	return 0;
+}
