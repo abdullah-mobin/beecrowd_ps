b94afc9a4b059625f74be8f47616c070554a8db2
Md Abdullah Al Mobin <112345262+abdullah-mobin@users.noreply.github.com>
2023-01-31 21:28:27 +0600

    Create 1157 - Divisors I.cpp

---
+++ 1157 - Divisors I.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int a;
+	cin >> a;
+	for (int i = 1; i <= a; i++)
+	{
+		if (a%i==0)
+		{
+			cout << i << endl;
+		}
+		
+	}
+
+	return 0;
+}
