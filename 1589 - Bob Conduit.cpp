2319d155a65183e0ee3c3b157d25cd8376f5ad78
abdullah-mobin <abdullahmobin1@yahoo.com>
2023-02-08 20:39:00 +0600

    1589 - Bob Conduit

---
+++ 1589 - Bob Conduit.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    while (a--)
+    {
+        int x, y;
+        cin >> x >> y;
+        cout << x + y << endl;
+    }
+
+    return 0;
+}
