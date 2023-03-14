0bc9cbcbbe3228f11242541e4986c092b692e3e8
abdullah-mobin <abdullahmobin1@yahoo.com>
2023-03-14 20:00:26 +0600

    2581 - I am Toorg! solved in cpp

---
+++ 2581 - I am Toorg!.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    string q;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        getline(cin,q);
+        cin.ignore();
+        cout<<"I am Toorg!"<<endl;
+    }
+    
+    return 0;
+}
